15c3c811c1f6e924de931127836ccd2cdc650fe1
G-J-Y <G-J-Y@users.noreply.github.com>
2021-03-26 01:00:53 -0400

    Update S_Puzzle.py

--- S_Puzzle.py
+++ S_Puzzle.py
@@ -35,13 +35,31 @@ def convertToMatrix(string, columns):
     return state
 
 
-def f():
+def output_one(f, s):
+    f.write("(")
+    for index in range(len(s)):
+        f.write("(")
+        for i in range(len(s[index])):
+            f.write("{}".format(s[index][i]))
+            if i < len(s[index])-1:
+                f.write(';')
+        if index < len(s)-1:
+            f.write(");")
+        else:
+            f.write(")")
+    f.write(")")
+
+def output_all(file, lst):
+    #with open(file, 'a') as file_obj:
+        for i in range(len(lst)):
+            output_one(file, lst[i])
+            if i < len(lst) - 1:
+                file.write('=>\n')
+
+
+def run_time():
    start_time = time.time()
-
-   # ... do something ...
-
    passed_time = time.time() - start_time()
-
    print(f'Total time (in seconds): {passed_time}')
 
 
@@ -111,13 +129,21 @@ def create_node(puzzle, parent, depth, cost):
     return Node(puzzle, parent, depth, cost)
 
 
-def dfs(start, goal):
+def dfs(start, goal, file1, file2):
     openList = []
     closeList = []
     puzzle_dict = []
 
+    file1.write("DFS\n")
+    file2.write("DFS\n")
+
     start_node = create_node(start,None,0,0)
     openList.append(start_node)
+
+    output_one(file1, start_node.puzzle)
+    file1.write("\n")
+    output_one(file2, start_node.puzzle)
+    file2.write("\n")
 
     puzzle_str = "".join(element for sub in start_node.puzzle for element in sub)
     puzzle_dict.append(puzzle_str)
@@ -139,18 +165,33 @@ def dfs(start, goal):
                 print("Find solution")
                 print(current_node.puzzle)
                 path = []
+                path_str = []
                 temp = current_node
                 print(temp.depth)
                 while True:
+                    # str list
                     current_str = convertMatrixToString(temp.puzzle)
-                    path.insert(0, current_str)
+                    path_str.insert(0, current_str)
+                    # puzzle list
+                    path.insert(0, temp.puzzle)
                     if temp.depth <= 0:
                         break
                     temp = temp.parent
 
-                print(path)
+                print("Path", path_str)
                 elapsed_time = time.time() - start_time
                 print(elapsed_time)
+                # output1
+                file1.write("Solution Path:\n")
+                output_all(file1, path)
+                file1.write("\n\n")
+                # output2
+                file2.write("Search Path:\n")
+                for i in range(len(closeList)):
+                    output_one(file2, closeList[i].puzzle)
+                    if i < len(closeList) - 1:
+                        file2.write(', ')
+                file2.write("\n\n")
                 return path
             else:
                 child_nodes = findChildNodes(current_node)
@@ -166,18 +207,28 @@ def dfs(start, goal):
                         openList.append(node)
         else:
             print("No solution")
+            file1.write("Solution Path: No solution, run out of states\n\n")
+            file2.write("Search Path: No solution, run out of states\n\n")
             return 'No solution'
 
 
 
-def iterativeDeepening(start, goal):
+def iterativeDeepening(start, goal, file1, file2):
     openList = []
     closeList = []
-    depth_limit = 1
+    depth_limit = 20
     puzzle_dict = []
+
+    file1.write("Iterative Deepening\n")
+    file2.write("Iterative Deepening\n")
 
     start_node = create_node(start,None,0,0)
     openList.append(start_node)
+
+    output_one(file1, start_node.puzzle)
+    file1.write("\n")
+    output_one(file2, start_node.puzzle)
+    file2.write("\n")
 
     puzzle_str = "".join(element for sub in start_node.puzzle for element in sub)
     puzzle_dict.append(puzzle_str)
@@ -199,18 +250,33 @@ def iterativeDeepening(start, goal):
                 print("Find solution")
                 print(current_node.puzzle)
                 path = []
+                path_str = []
                 temp = current_node
                 print(temp.depth)
                 while True:
+                    # str list
                     current_str = convertMatrixToString(temp.puzzle)
-                    path.insert(0, current_str)
+                    path_str.insert(0, current_str)
+                    # puzzle list
+                    path.insert(0, temp.puzzle)
                     if temp.depth <= 0:
                         break
                     temp = temp.parent
 
-                print(path)
+                print("Path", path_str)
                 elapsed_time = time.time() - start_time
                 print(elapsed_time)
+                # output1
+                file1.write("Solution Path:\n")
+                output_all(file1, path)
+                file1.write("\n\n")
+                # output2
+                file2.write("Search Path:\n")
+                for i in range(len(closeList)):
+                    output_one(file2, closeList[i].puzzle)
+                    if i < len(closeList) - 1:
+                        file2.write(', ')
+                file2.write("\n\n")
                 return path
             else:
                 if current_node.depth < depth_limit:
@@ -229,19 +295,35 @@ def iterativeDeepening(start, goal):
 
                 if len(openList) == 0:
                     print("No solution, run out of states")
+                    file1.write("Solution Path: No solution, run out of states\n\n")
+                    file2.write("Search Path: No solution, run out of states\n\n")
                     return 'No solution'
         else:
             print("No solution")
+            file1.write("Solution Path: No solution, run out of time\n\n")
+            file2.write("Search Path: No solution, run out of time\n\n")
             return 'No solution'
 
 
-def AStar(start, goal, heuristic):
+def AStar(start, goal, heuristic, file1, file2):
     openList = []
     closeList = []
     puzzle_list = []
 
+    if heuristic == 1:
+        file1.write("A*, h1\n")
+        file2.write("A*, h1\n")
+    if heuristic == 2:
+        file1.write("A*, h2\n")
+        file2.write("A*, h2\n")
+
     start_node = create_node(start,None,0,0)
     openList.append(start_node)
+
+    output_one(file1, start_node.puzzle)
+    file1.write("\n")
+    output_one(file2, start_node.puzzle)
+    file2.write("\n")
 
     puzzle_str = "".join(element for sub in start_node.puzzle for element in sub)
     puzzle_list.append(puzzle_str)
@@ -269,18 +351,33 @@ def AStar(start, goal, heuristic):
                 print("Find solution")
                 print(current_node.puzzle)
                 path = []
+                path_str = []
                 temp = current_node
                 print("Depth", temp.depth)
                 while True:
+                    # str list
                     current_str = convertMatrixToString(temp.puzzle)
-                    path.insert(0, current_str)
+                    path_str.insert(0, current_str)
+                    # puzzle list
+                    path.insert(0,temp.puzzle)
                     if temp.depth <= 0:
                         break
                     temp = temp.parent
 
-                print("Path", path)
+                print("Path", path_str)
                 elapsed_time = time.time() - start_time
                 print(elapsed_time)
+                # output1
+                file1.write("Solution Path:\n")
+                output_all(file1, path)
+                file1.write("\n\n")
+                # output2
+                file2.write("Search Path:\n")
+                for i in range(len(closeList)):
+                    output_one(file2, closeList[i].puzzle)
+                    if i < len(closeList) - 1:
+                        file2.write(', ')
+                file2.write("\n\n")
                 return path
             else:
                 child_nodes = findChildNodes(current_node)
@@ -296,6 +393,8 @@ def AStar(start, goal, heuristic):
                         openList.append(node)
         else:
             print("No solution")
+            file1.write("Solution Path: No solution, run out of time\n\n")
+            file2.write("Search Path: No solution, run out of time\n\n")
             return 'No solution'
 
 
@@ -379,7 +478,32 @@ goal = convertToMatrix(goal_state, 3)
 start_puzzle = convertToMatrix(start_state, 3)
 print()
 
-#dfs(start_puzzle, goal)
-iterativeDeepening(start_puzzle, goal)
-#AStar(start_puzzle, goal, 1)
-#AStar(start_puzzle, goal, 2)
+# output
+output_DFS_solution = open("Output_DFS_solution.txt", "w")
+output_DFS_search = open("Output_DFS_search.txt", "w")
+
+output_Depth_solution = open("Output_Depth_solution.txt", "w")
+output_Depth_search = open("Output_Depth_search.txt", "w")
+
+output_h1_solution = open("Output_h1_solution.txt", "w")
+output_h1_search = open("Output_h1_search.txt", "w")
+
+output_h2_solution = open("Output_h2_solution.txt", "w")
+output_h2_search = open("Output_h2_search.txt", "w")
+
+dfs(start_puzzle, goal, output_DFS_solution, output_DFS_search)
+
+iterativeDeepening(start_puzzle, goal, output_Depth_solution, output_Depth_search)
+
+AStar(start_puzzle, goal, 1, output_h1_solution, output_h1_search)
+
+AStar(start_puzzle, goal, 2, output_h2_solution, output_h2_search)
+
+output_DFS_solution.close()
+output_DFS_search.close()
+output_Depth_solution.close()
+output_Depth_search.close()
+output_h1_solution.close()
+output_h1_search.close()
+output_h2_solution.close()
+output_h2_search.close()
